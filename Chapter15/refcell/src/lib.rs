884bbdabbb23b0cd938820252649d5ef3b7a01ee
Devin Miller <devinmiller@codeonthebrain.com>
2018-09-24 18:55:38 -0700

    Moved folders, continued Chapter 15.

--- Chapter15/refcell/src/lib.rs
+++ Chapter15/refcell/src/lib.rs
@@ -1,14 +1,14 @@
 pub trait Messenger {
-    fn Send(&self, msg: &str);
+    fn send(&self, msg: &str);
 }
 
-pub struct LimitTracker<T: Messenger> {
-    messenger: &T,
+pub struct LimitTracker<'a, T: 'a + Messenger> {
+    messenger: &'a T,
     value: usize,
     max: usize
 }
 
-impl<T> LimitTracker<T>
+impl<'a, T> LimitTracker<'a, T>
     where T: Messenger {
 
     pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
@@ -37,5 +37,31 @@ impl<T> LimitTracker<T>
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::cell::RefCell;
     
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger { sent_messages: RefCell::new(vec![]) }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+
+    #[test]
+        fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
 }
