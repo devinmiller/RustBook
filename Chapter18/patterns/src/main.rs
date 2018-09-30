dac31c836e2f8c781f4cd668d5eeda41aa486f74
Devin Miller <devinmiller@codeonthebrain.com>
2018-09-30 14:14:57 -0700

    Started Chapter 18.

---
+++ Chapter18/patterns/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {
+    println!("Hello, world!");
+
+    if_let();
+    while_let_loop();
+    for_loops();
+}
+
+fn if_let() {
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {}, as the background", color);
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+}
+
+fn while_let_loop() {
+    let mut stack = vec![1, 2, 3];
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+}
+
+fn for_loops() {
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+}
