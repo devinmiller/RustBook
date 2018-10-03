fb4ad8332eed2f0f33393c636fad9122e3f5c146
Devin Miller <devinmiller@codeonthebrain.com>
2018-10-02 19:01:03 -0700

    Continued Chapter 18.

--- Chapter18/patterns/src/main.rs
+++ Chapter18/patterns/src/main.rs
@@ -4,6 +4,154 @@ fn main() {
     if_let();
     while_let_loop();
     for_loops();
+
+    matching_literals();
+    matching_variables();
+    matching_multiples();
+    matching_ranges();
+
+    destructuring_structs();
+    destructuring_enums();
+
+    nested_destructuring();
+}
+
+fn nested_destructuring() {
+    enum Color {
+        Rgb(i32, i32, i32),
+        Hsv(i32, i32, i32)
+    }
+
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(Color),
+    }
+
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!(
+                "Change the color to red {}, green {}, and blue {}",
+                r,
+                g,
+                b
+            )
+        },
+        Message::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!(
+                "Change the color to hue {}, saturation {}, and value {}",
+                h,
+                s,
+                v
+            )
+        },
+        _ => ()
+    }
+}
+
+fn destructuring_enums() {
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32)
+    }
+
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.")
+        },
+        Message::Move {x, y} => {
+            println!(
+                "Move in the x direction {} and in the y direction {}",
+                x,
+                y
+            );
+        },
+        Message::Write(text) => {
+            println!("Text message: {}", text)
+        },
+        Message::ChangeColor(r, g, b) => {
+            println!(
+                "Change the color to red {}, green {}, and blue {}",
+                r,
+                g,
+                b
+            )
+        }
+    }
+}
+
+fn destructuring_structs() {
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    let p = Point { x: 0, y: 15 };
+    let Point { x: a, y: b } = p;
+
+    assert_eq!(0, a);
+    assert_eq!(15, b);
+
+    let Point { x, y } = p;
+
+    assert_eq!(0, x);
+    assert_eq!(15, y);
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+}
+
+fn matching_ranges() {
+    let x = 5;
+
+    match x {
+        1...5 => println!("one through five"),
+        _ => println!("something else")
+    }
+}
+
+fn matching_multiples() {
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything")
+    }
+}
+
+fn matching_variables() {
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+}
+
+fn matching_literals() {
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
 }
 
 fn if_let() {
